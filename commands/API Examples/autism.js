53b84b23449f9d5e67bcd857387500fc0eaaa03f
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-10-21 13:55:16 -0400

    autism

---
+++ commands/API Examples/autism.js
@@ -0,0 +1,30 @@
+const { Command } = require("klasa");
+const { MessageAttachment } = require("discord.js");
+const superagent = require("superagent"); 
+
+class Autism extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Example command for GET /api/abandon",
+      name: "autism",
+      usage: "[text:str]"
+    });
+  }
+
+  async run(msg, [text]) {
+    const start = Date.now();
+    const res = await superagent.get("https://bananapi.ml/api/autism")
+      .query({ text: text }) // Params
+      .set({ Authorization: this.client.config.apikey });
+    const end = Date.now();
+    const attachment = new MessageAttachment(res.body, "autism.png");
+    const message = `**URL:** https://bananapi.ml/api/autism\n**API Response Time:** ${end - start} ms`;
+    await msg.send(message, attachment);
+  }
+}
+
+
+
+module.exports = Autism;
+
+
