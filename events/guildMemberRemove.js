e3286220bf2f8c59b97613751750c48060714a8f
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-11-11 15:41:28 -0500

    e

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,15 @@
+const { Event } = require("klasa");
+const { MessageEmbed } = require("discord js");
+
+class GuildMemberRemove extends Event {
+
+  async run(member) {
+    const res = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [member.user.id]);
+    if (!res.rows.length) return;
+    await this.client.db.query("DELETE FROM tokens WHERE userid = $1", [member.user.id]);
+    await this.client.channels.get("492085452430639124").send(`**${member.user.tag}** left the server. I revoked their BananAPI key.`);
+
+  }
+}
+
+module.exports = GuildMemberRemove;
