0339fb2f13c42210812c074c7f05ff3f0317aad3
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-11-16 16:56:52 -0500

    ms

--- index.js
+++ index.js
@@ -129,9 +129,6 @@ app.get("/libs", async (req, res) => {
   });
 });
 
-app.get("/docs", async (req, res) => {
-  res.render("docs/index.html")
-})
 
 app.get("/login", passport.authenticate("discord", {
   failureRedirect: "/"
