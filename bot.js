c6639b1e2eedda15195e38f1bd80c7a7adbfbbcc
Main_User_Drive <hi.pixelpikachu@hotmail.com>
2018-12-06 16:19:29 +0000

    Made pl 1 use the BananAPI Developer role
    Made pl 1 use the BananAPI Developer role instead of preset.

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const config = require("./config.json");
 
 const permissionLevels = new PermissionLevels()
   .add(0, () => true)
-  .add(1, (_, m) => config.devs.includes(m.author.id), { fetch: true })
+  .add(1, (_, m) => config.DevRole.includes(m.roles), { fetch: true }) //Note to banana: add a section in config.json called "DevRole" and the value be "520267421232267284", kthx
   .add(10, (_, m) => m.author.id === "277981712989028353" || m.author.id == "302604426781261824" || m.author.id === "292690616285134850" || m.author.id === "304737539846045696", { break: true, fetch: true });
 
 
