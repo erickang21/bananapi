9be420c1f2ba3260fe01bc926602fead8a62a3a0
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-12-27 09:34:40 -0500

    e

--- commands/Dev/gettoken.js
+++ commands/Dev/gettoken.js
@@ -15,6 +15,6 @@ module.exports = class extends Command {
     const res = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [user.id]);
     if (!res.rows.length) return msg.send(`**${user.tag}** doesn't have a token yet! How sad is that?`);
     await msg.author.send(`You are viewing **${user.tag}**'s token.\n\n\`${res.rows[0].token}\``);
-    await msg.send("Got your token! It's in your DMs.");
+    await msg.send(`I got **${user.tag}**'s token for you! Check our DMs for it.`);
   }
 };
