6e8cacf8ccf384babe7826f2e5d91b72d6709a99
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-12-27 08:39:35 -0500

    test

--- commands/Dev/accept.js
+++ commands/Dev/accept.js
@@ -13,12 +13,13 @@ class Accept extends Command {
 
   async run(msg, [user]) {
     const token = crypto.randomBytes(32).toString("hex");
+    const test = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [msg.author.id]);
+    if (test.rows.length) return msg.send("It looks like this user already has a token!");
     const res = await this.client.db.query("INSERT INTO tokens (userid, token) VALUES ($1, $2)", [user.user.id, token]);
-    if (!res.rows.length) return msg.send("An error occurred. The user may already have a token.");
     await user.send(`Hey there! I got some good news for you.\n\nYou've been approved for a token for BananAPI! Here is it below:\n\`${token}\`\n\nBe sure to pass it as an Authorization header to complete your requests.\n\nAlright, won't bother you any more. Enjoy your day!`);
     const role = msg.guild.roles.get("511256854886875145");
     user.roles.add(role);
-    await msg.send(`Success, gave **${user.user.tag}** a token. They'll be happy for sure.`);
+    await msg.send(`Success! I gave **${user.user.tag}** a token. They'll be happy for sure.`);
   }
 }
 
--- commands/Dev/token.js
+++ commands/Dev/token.js
@@ -5,12 +5,20 @@ class Token extends Command {
   constructor(...args) {
     super(...args, {
       description: "View your BananAPI token.",
-      name: "token"
+      name: "token",
+      usage: "[user:str]"
     });
   }
 
-  async run(msg) {   
-    const res = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [msg.author.id]);
+  async run(msg, [user]) {   
+    let member;
+    if (user) {
+      if (!msg.hasAtLeastPermissionLevel(1)) return msg.send("What are you trying to do? You can't view other people's tokens unless you're a dev. Which you're not.");
+      member = user;
+    } else {
+      member = msg.author;
+    }
+    const res = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [member.id]);
     if (!res.rows.length) return msg.send("Looks like you don't have a token yet. Get one, with `b.apply`.");
     await msg.author.send(`Here's your token! Just like you asked.\n\n\`${res.rows[0].token}\``);
     await msg.send("Got your token! It's in your DMs.");
