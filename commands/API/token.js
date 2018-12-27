0ab8ab096772dabecbfc2a707808545da3f12df4
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-12-27 09:27:39 -0500

    gettoken

--- commands/API/token.js
+++ commands/API/token.js
@@ -5,20 +5,12 @@ class Token extends Command {
   constructor(...args) {
     super(...args, {
       description: "View your BananAPI token.",
-      name: "token",
-      usage: "[user:user]"
+      name: "token"
     });
   }
 
-  async run(msg, [user]) {   
-    let member;
-    if (user) {
-      if (!msg.hasAtLeastPermissionLevel(1)) return msg.send("What are you trying to do? You can't view other people's tokens unless you're a dev. Which you're not.");
-      member = user;
-    } else {
-      member = msg.author;
-    }
-    const res = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [member.id]);
+  async run(msg) {   
+    const res = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [msg.author.id]);
     if (!res.rows.length) return msg.send("Looks like you don't have a token yet. Get one, with `b.apply`.");
     await msg.author.send(`Here's your token! Just like you asked.\n\n\`${res.rows[0].token}\``);
     await msg.send("Got your token! It's in your DMs.");
---
+++ commands/Dev/gettoken.js
@@ -0,0 +1,20 @@
+const { Command } = require("klasa");
+
+module.exports = class extends Command {
+
+  constructor(...args) {
+    super(...args, {
+      name: "gettoken",
+      description: "Get the token of others.",
+      permissionLevel: 10,
+      usage: "[user:user]"
+    });
+  }
+
+  async run(msg, [user]) {
+    const res = await this.client.db.query("SELECT * FROM tokens WHERE userid = $1", [user.id]);
+    if (!res.rows.length) return msg.send(`**${user.tag}** doesn't have a token yet! How sad is that?`);
+    await msg.author.send(`You are viewing **${user.tag}**'s token.\n\n\`${res.rows[0].token}\``);
+    await msg.send("Got your token! It's in your DMs.");
+  }
+};
